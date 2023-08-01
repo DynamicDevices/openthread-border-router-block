0bddd7f3b2fab36e3ab551a22e12512124aee0b4
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-08-01 19:05:29 +0100

    Support configuration of MULTICAST_IPV6 address
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- openthread-otbr/run-mqtt-sngateway.sh
+++ openthread-otbr/run-mqtt-sngateway.sh
@@ -6,6 +6,8 @@ sed -i "s/ENV_BROKER_SECURE_PORT/${BROKER_SECURE_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_PORT/${GATEWAY_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_IPV6_PORT/${GATEWAY_IPV6_PORT}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_IF/${MULTICAST_IPV6_IF}/" gateway.conf
+sed -i "s/ENV_MULTICAST_IPV6/${MULTICAST_IPV6}/" gateway.conf
+sed -i "s/ENV_MULTICAST_HOPS/${MULTICAST_IPV6_HOPS}/" gateway.conf
 sed -i "s/ENV_MULTICAST_HOPS/${MULTICAST_IPV6_HOPS}/" gateway.conf
 
 export LD_LIBRARY_PATH=.
