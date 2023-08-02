09c12d85d0cfbc1baba4a5ccbcff2127cd7d4dde
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-08-02 14:56:30 +0100

    Changes to fix MQTT-SN issues with SEARCHGW multicasts
    The multicasting wasn't working with the upstream PAHO code
    
    I've forked this and made changes from the code here
    
    https://github.com/kyberpunk/paho.mqtt-sn.embedded-c
    
    My code is here:
    
    https://github.com/DynamicDevices/paho.mqtt-sn.embedded-c/tree/ajl/fix-debug-ipv6-addr-output
    
    This modifies the broadcast function. Frankly I'm not entirely
    clear on why this now works but it does under testing
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- openthread-otbr/run-mqtt-sngateway.sh
+++ openthread-otbr/run-mqtt-sngateway.sh
@@ -5,6 +5,7 @@ sed -i "s/ENV_BROKER_PORT/${BROKER_PORT}/" gateway.conf
 sed -i "s/ENV_BROKER_SECURE_PORT/${BROKER_SECURE_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_PORT/${GATEWAY_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_IPV6_PORT/${GATEWAY_IPV6_PORT}/" gateway.conf
+sed -i "s/ENV_MULTICAST_IPV6_PORT/${MULTICAST_IPV6_PORT}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_IF/${MULTICAST_IPV6_IF}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6/${MULTICAST_IPV6}/" gateway.conf
 sed -i "s/ENV_MULTICAST_HOPS/${MULTICAST_IPV6_HOPS}/" gateway.conf
