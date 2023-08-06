b75f30a6ec5897c8fd9fc1b1de620715ced3664b
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-08-06 14:56:01 +0100

    Fix sed ordering
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- openthread-otbr/run-mqtt-sngateway.sh
+++ openthread-otbr/run-mqtt-sngateway.sh
@@ -7,8 +7,8 @@ sed -i "s/ENV_GATEWAY_PORT/${GATEWAY_PORT}/" gateway.conf
 sed -i "s/ENV_GATEWAY_IPV6_PORT/${GATEWAY_IPV6_PORT}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_PORT/${MULTICAST_IPV6_PORT}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_IF/${MULTICAST_IPV6_IF}/" gateway.conf
-sed -i "s/ENV_MULTICAST_IPV6/${MULTICAST_IPV6}/" gateway.conf
 sed -i "s/ENV_MULTICAST_IPV6_HOPS/${MULTICAST_IPV6_HOPS}/" gateway.conf
+sed -i "s/ENV_MULTICAST_IPV6/${MULTICAST_IPV6}/" gateway.conf
 
 export LD_LIBRARY_PATH=.
 
