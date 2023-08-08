f43d007e5ebaccf365238d17e0e96aeac123262e
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-08-08 12:30:18 +0100

    Extend timeouts on start as we're still seeing form failures
    on bootup. Need a way to detect that whatever we need to be
    running is actually running rather than just waiting
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- openthread-otbr/docker_entrypoint.sh
+++ openthread-otbr/docker_entrypoint.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # Wait for DBUS to be up after boot
-sleep 10
+echo Waiting for system to come up
+sleep 60
 
 # Run up OTBR services
+echo Running OTBR services
 /app/etc/docker/docker_entrypoint.sh &
 
 # TODO: Work out how to know OTBR is up
-sleep 10
+sleep 30
 
 # Optionally form the network
+echo Forming Network
 /app/form-network.sh
 
-# Run MQTT-SN Gateway
+# Optionally run MQTT-SN gateway in this container
+echo Running MQTT-SN gateway
 /app/run-mqtt-sngateway.sh
 
 # Block so the container doesn't exit
+echo Sleeping...
 while [ 1 ]
 do
  sleep 10
